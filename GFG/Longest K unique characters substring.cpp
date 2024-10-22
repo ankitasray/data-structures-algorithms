4e670fd993a9f7e67227ce8e95f75e7c717c6afd
Ankita Sinha Ray <109851529+ankitasray@users.noreply.github.com>
2024-10-22 20:44:35 +0530

    Create Longest K unique characters substring.cpp

---
+++ GFG/Longest K unique characters substring.cpp
@@ -0,0 +1,32 @@
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        int i=0 ,j=0;
+        int mx= INT_MIN;
+        
+        unordered_map<char,int>mp;
+        
+        while(j < s.size()) {
+            mp[s[j]]++;
+            
+            if(mp.size() < k){
+                j++;
+            }
+            else if(mp.size() == k){
+                mx = max(mx, j-i+1);
+                j++;
+            }
+            else if(mp.size() > k) {
+                while(mp.size()>k){
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0){
+                        mp.erase(s[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        return mx;
+    }
+};
