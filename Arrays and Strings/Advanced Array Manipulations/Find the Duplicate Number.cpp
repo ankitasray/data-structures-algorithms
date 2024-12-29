a54d346688785a625a518a87bc1800781282c064
Ankita Sinha Ray <109851529+ankitasray@users.noreply.github.com>
2024-12-30 00:28:23 +0530

    Update Find the Duplicate Number.cpp

--- Arrays and Strings/Advanced Array Manipulations/Find the Duplicate Number.cpp
+++ Arrays and Strings/Advanced Array Manipulations/Find the Duplicate Number.cpp
@@ -26,3 +26,23 @@ public:
         return -1;
     }
 };
+O(N) O(1)
+
+//using map (counting)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto& it:mp){
+            if(it.second>1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+O(N) O(N)
