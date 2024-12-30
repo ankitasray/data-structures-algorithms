163338c648fcc16718aecae88431bb2d16b29b4c
Ankita Sinha Ray <109851529+ankitasray@users.noreply.github.com>
2024-12-30 15:10:15 +0530

    Create NextPermutation.cpp

---
+++ Arrays and Strings/Advanced Array Manipulations/NextPermutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+    }
+};
+---------------------------------------------
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int ind = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        for(int i=n-1; i>=ind && ind != -1; i--){
+            if(nums[i] > nums[ind]){
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin() + ind + 1, nums.end());
+    }
+};
+
+
